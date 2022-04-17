ecf0f1e9d348127d1dbd5b77d8e7f46165825959
Estevam Albuquerque <estevamgalvao1@gmail.com>
2022-04-17 05:18:31 -0300

    just lefting semaphore for prints

--- board/src/game.cpp
+++ board/src/game.cpp
@@ -18,22 +18,31 @@ int main()
     p_id2 = fork();
     if (p_id2 == 0) {execvp(PLAYER_EXEC_PATH, player_id2);}
 
-    while (int i = 40 > 0)
+    while (true)
     {
         tabuleiro.PrintBoard();
         // sleep(1);
-        i--;
         if (tabuleiro.GetSlotss()[BOARD_SIZE*BOARD_SIZE] != 0 
         && tabuleiro.GetSlotss()[(BOARD_SIZE*BOARD_SIZE) + 1] != 0) {
             break;
         }
     }
-    
-    printf("Acabou o jogo.\n");
 
     /* waiting the return from the child processes */
     waitpid(p_id1, &p_status1, 0);
     waitpid(p_id2, &p_status2, 0);
+
+    if (tabuleiro.GetSlotss()[BOARD_SIZE*BOARD_SIZE] > tabuleiro.GetSlotss()[BOARD_SIZE*BOARD_SIZE + 1]) {
+        printf("\nAcabou o jogo. O jogador 1 é o campeão.\n");
+    }
+    else if (tabuleiro.GetSlotss()[BOARD_SIZE*BOARD_SIZE] < tabuleiro.GetSlotss()[BOARD_SIZE*BOARD_SIZE + 1]) {
+        printf("\nAcabou o jogo. O jogador 2 é o campeão.\n");
+    }
+    else {
+        printf("\nAcabou o jogo. Os jogadores empataram.\n");
+    }
+
+    printf("Acabou o jogo.\n");
 
     tabuleiro.~Board();
     return 0;
